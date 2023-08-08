6a04b1538ea8cbbaddc8677ccfb2db574471d42b
Blokotec <lucas.souza@blokotecnologia.com.br>
2023-08-08 16:24:06 -0300

    envio da index

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Suporte</title>
+    </head>
+    <body>
+        <div width="100%">
+        <script>
+	        window.fwSettings={
+	        'widget_id':150000004165
+	        };
+	        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
+        </script>
+        <script type='text/javascript' src='https://widget.freshworks.com/widgets/150000004165.js' async defer></script>
+
+            <script> 
+                FreshworksWidget('open');
+                FreshworksWidget('hide', 'launcher');    
+            </script>
+        </div>
+        
+    </body>
+</html>
